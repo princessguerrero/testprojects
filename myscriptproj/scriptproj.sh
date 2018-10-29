14c83d46425e3d98b0dc74cf525680b4de41ae45
Princess Guerrero <princessguerrero@ac.c4q.nyc>
2018-10-28 20:56:58 -0400

    changed code to goals for notification

--- myscriptproj/scriptproj.sh
+++ myscriptproj/scriptproj.sh
@@ -2,10 +2,10 @@ if [ $1 = "goals" ];
     then
     export DISPLAY=:0 && xdg-open https://leetcode.com/problemset/algorithms/?difficulty=Easy&listId=wpwgkgt 
     xdg-open https://inforlms.certpointsystems.com/lms/basicportal/eap/en-US/
-    xdg-open goals.txt
+    xdg-open https://raw.githubusercontent.com/princessguerrero/testprojects/master/myscriptproj/goals.txt
 fi
 
-notify-send "Code" ; sleep 5 ; killall notify-osd
+notify-send "Goals!!!" ; sleep 5 ; killall notify-osd
 
 # if [ $1 = "leet" ];
 #     then export DISPLAY=:0 && xdg-open https://leetcode.com/problemset/algorithms/?difficulty=Easy&listId=wpwgkgt 
