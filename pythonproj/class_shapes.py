9a697700b95f250674365abb840152bb3b07070d
Princess Guerrero <princessguerrero@ac.c4q.nyc>
2018-11-03 19:12:17 -0400

    added class Rectangle

--- pythonproj/class_shapes.py
+++ pythonproj/class_shapes.py
@@ -16,3 +16,14 @@ class Shapes:
 
 class Square(Shapes):
     pass
+
+class Rectangle(Shapes):
+    def __init__(self, length, width):
+        self.length = length
+        self.width = width
+        
+    def area(self):
+        return self.length * self.width
+    
+    def perimeter(self):
+        return 2*self.length + 2*self.width
