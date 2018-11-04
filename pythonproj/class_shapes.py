1e388d8ef7922d2bf7c2c27e671f531569b98105
Princess Guerrero <princessguerrero@ac.c4q.nyc>
2018-11-04 11:40:36 -0500

    added command line interface

--- pythonproj/class_shapes.py
+++ pythonproj/class_shapes.py
@@ -6,13 +6,15 @@ class Shapes:
         self.length_of_sides = length_of_sides
         
     def area(self):
+        print("The area is")
         return self.length_of_sides * self.length_of_sides
     
     def perimeter(self):
+        print("The perimeter is")
         return 4*self.length_of_sides
     
-    def num_of_sides(self):
-        return 4
+    # def num_of_sides(self):
+    #     return 4
 
 class Square(Shapes):
     pass
@@ -64,3 +66,46 @@ class Circle(Shapes):
     
     def perimeter(self):
         return 2*self.pi*self.r
+
+def choose_shape(id):
+
+    if id == 1:
+        print("Enter sides")
+        sides = int(input())
+        x = Square(sides)
+        print(x.perimeter())
+        print(x.area())
+    elif id == 2:
+        print("Enter a length")
+        length = int(input())
+        print("Enter a width")
+        width = int(input())
+        x = Rectangle(length, width)
+        print("Perimeter")
+        print(x.perimeter())
+        print("Area")
+        print(x.area())
+    elif id == 3:
+        return Diamond()
+    elif id == 4:
+        return Triangle()
+    elif id == 5:
+        return Circle()
+    else:
+        print("Please choose a valid number")
+        return shape_menu()
+
+def shape_menu():
+    print("Select A Shape")
+    print("1) Square")
+    print("2) Rectangle")
+    print("3) Diamond")
+    print("4) Triangle")
+    print("5) Circle")
+
+    id = int(input("Choose a shape by selecting the number "))
+    return choose_shape(id)
+
+print("Welcome to Shape Area and Perimeter")
+print(shape_menu())
+text = input()
